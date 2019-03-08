a9986a431dfca9c98f9ed031effaa3327002add5
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-08 09:11:29 -0500

    Imported 'createStore' from 'redux' into index.js (src) and created a 'const store' variable with a passed in argument of 'reducer'

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
+
+
+
+const store = createStore(reducer);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
