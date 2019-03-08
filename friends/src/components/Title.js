c4889579e510e2bc308a1835d73d6d7315a21e2e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-08 10:31:37 -0500

    Built a navbar inside of <Title /> component and updated layout/styles in App.css

--- friends/src/components/Title.js
+++ friends/src/components/Title.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 const Title = () => {
     return (
-        <div>
-            <h1>My Friends App (Hardcoded Title)</h1>
+        <div className="Title-container">
+            <h1 className="Title-heading">My Friends App</h1>
+            <ul className="Title-navbar">
+                <a 
+                    className="Title-navbar-link" 
+                    href="http://localhost:3000">
+                    Home
+                </a>
+                <a 
+                    className="Title-navbar-link" 
+                    href="http://localhost:3000"
+                >
+                    Friends
+                </a>
+            </ul>
         </div>
     )        
 };
