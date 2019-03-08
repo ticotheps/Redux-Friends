913a6fcdf6202b6d5b0055c9ca8dd4b8f7620b15
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-08 09:39:09 -0500

    Built out <Title /> component and got it to render hardcoded <h1> element to the screen

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
+import Title from './components/Title';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        My Friends App
         <Title />
       </div>
-    );
+    )
   }
 }
 
